2f3bf6faa1890909edbd41ae8ed00f388fe8af12
m33x <maximilian.golla@gmail.com>
2020-09-26 13:34:32 +0200

    Improved navigation

--- js/custom/max.js
+++ js/custom/max.js
@@ -5,6 +5,13 @@ $(document).ready(function () {
     'use strict';
 
     $(".smooth-scroll").on('click', 'a', function (e) {
+        // Iterate over all and remove active class
+        $.each($(".nav-item"), function () {
+            $(this).removeClass('active');
+        });
+        // Add active class on clicked <li>
+        $(this).closest(".nav-item").addClass('active');
+        // Below is smoothscrolling
         var link = $(this).attr('href');
         // Is there an HTML anchor in the hyperlink?
         if (link.indexOf("#") >= 0) {
